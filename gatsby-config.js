e3a0d7ca7db055d633a4d94fbe2221fa4f6b09a8
Jason Huang <jasonhuang20035@gmail.com>
2020-06-27 22:12:30 -0400

    Update Manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#edf2f7`,
-        display: `browser`,
+        display: `minimal-ui`,
         icon: `src/images/icon.svg`,
         icon_options: {
           purpose: `maskable`,
