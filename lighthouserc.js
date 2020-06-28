107a9a47164ce39f6654df010613a8d62c0925ab
Jason Huang <jasonhuang20035@gmail.com>
2020-06-27 22:56:56 -0400

    Increase testing due to inconsistency

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,7 @@ module.exports = {
     collect: {
       url: ["http://localhost/index.html"],
       staticDistDir: "public",
-      numberOfRuns: 2,
+      numberOfRuns: 5,
     },
     assert: {
       preset: "lighthouse:recommended",
