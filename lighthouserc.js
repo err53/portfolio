5674530930a1214ea7b2dfd9dd522568b3200c56
Jason Huang <jasonhuang20035@gmail.com>
2020-06-27 22:19:06 -0400

    Update Lighthouse Config

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,9 +3,29 @@ module.exports = {
     collect: {
       url: ["http://localhost/index.html"],
       staticDistDir: "public",
+      numberOfRuns: 2,
+      staticDistDir: "./public",
     },
     assert: {
       preset: "lighthouse:recommended",
+      assertions: {
+        "first-contentful-paint": [
+          "warn",
+          {
+            maxNumericValue: 2500,
+            aggregationMethod: "optimistic",
+          },
+        ],
+        interactive: [
+          "warn",
+          {
+            maxNumericValue: 5000,
+            aggregationMethod: "optimistic",
+          },
+        ],
+        "uses-long-cache-ttl": "off",
+        "uses-http2": "off",
+      },
     },
     upload: {
       target: "temporary-public-storage",
