840adeef7034851c9550b4ff770cc79cde0e2184
Jason Huang <jasonhuang20035@gmail.com>
2020-07-05 18:52:14 -0400

    Replace Buttons with Draft Description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,16 +15,22 @@ const Home = () => {
         title="Jason Huang"
         description="High School Student, Software Developer, Linux Enthusiast"
       />
-
-      <Flex direction="column" justify="center" margin="1em" width="100%">
-        <Button as={Link} to="/projects/" margin="1em">
-          Projects
-        </Button>
-
-        <Button as={Link} to="/resume/" margin="1em">
-          Resume
-        </Button>
-      </Flex>
+      <Text>
+        Hi, I'm Jason. I'm a fan of technology, but try to be mindful of the
+        increasing privacy concerns surrounding it.
+      </Text>
+      <Text>
+        I have been programming since ~2015. I primarily do frontend
+        development, but have experience programming for backend. I also have a
+        hobby of learning about SysAdmin and DevOps. I wouldn't call myself a
+        full-stack developer, since I don't think I have enough experience to
+        qualify as such just yet.
+      </Text>
+      <Text>
+        Aside from programming, I'm an advocate for Linux and OSS, and love to
+        learn more about how the tools I use work. I'm also interested in
+        compuiter security, and have been known to do the occasional CTF.
+      </Text>
     </Layout>
   )
 }
