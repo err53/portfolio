26f28fc60f0890faf95ed1bb349319d91b82a2d0
Jason Huang <jasonhuang20035@gmail.com>
2020-07-08 16:29:17 -0400

    Fix Object Nest Order

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,13 +1,13 @@
-import React from "react"
-import Layout from "../components/layout"
-import Hero from "../components/hero"
-import SEO from "../components/seo"
-import Navbar from "../components/navbar"
-import { graphql, Link as GatsbyLink } from "gatsby"
-import { Stack, List, Link, ListItem, Heading, Text } from "@chakra-ui/core"
+import React from "react";
+import Layout from "../components/layout";
+import Hero from "../components/hero";
+import SEO from "../components/seo";
+import Navbar from "../components/navbar";
+import { graphql, Link as GatsbyLink } from "gatsby";
+import { Stack, List, Link, ListItem, Heading, Text } from "@chakra-ui/core";
 
 const Projects = ({ data }) => {
-  const { edges: projects } = data.allMdx
+  const { edges: projects } = data.allMdx;
   return (
     <Layout>
       <SEO title="Projects" />
@@ -20,17 +20,20 @@ const Projects = ({ data }) => {
         <List>
           {projects.map(({ node: project }) => (
             <ListItem key={project.id}>
-              <Link as={GatsbyLink} to={project.fields.slug}>
-                <Heading>{project.frontmatter.title}</Heading>
-              </Link>
+              <Heading>
+                <Link as={GatsbyLink} to={project.fields.slug}>
+                  {project.frontmatter.title}
+                </Link>
+              </Heading>
+
               <Text>{project.excerpt}</Text>
             </ListItem>
           ))}
         </List>
       </Stack>
     </Layout>
-  )
-}
+  );
+};
 
 export const pageQuery = graphql`
   query projectIndex {
@@ -49,6 +52,6 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
 
-export default Projects
+export default Projects;
