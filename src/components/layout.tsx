2b186bbb733b353cc8f62b9504d71eb29b797df2
Jason Huang <jasonhuang20035@gmail.com>
2020-07-08 18:12:25 -0400

    Fix Initial Load Not Filling Screen

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,30 +1,31 @@
-import React from "react"
-import { Stack, Text, Link } from "@chakra-ui/core"
-import Socials from "./socials"
+import React from "react";
+import { Flex, Stack, Text, Link } from "@chakra-ui/core";
+import Socials from "./socials";
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <Stack
-      padding="4"
-      height="100vh"
-      justify="space-between"
-      align="center"
-      maxWidth="3xl"
-      mx="auto"
-    >
-      <Stack as="body" width="100%">
-        {children}
+    <Flex justifyContent="center" width="100vw">
+      <Stack
+        padding="4"
+        height="100vh"
+        justify="space-between"
+        align="center"
+        maxWidth="3xl"
+      >
+        <Stack as="body" width="100%">
+          {children}
+        </Stack>
+        <Stack as="footer" width="100%" align="center">
+          <Socials />
+          <Text>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <Link href="https://www.gatsbyjs.org">Gatsby</Link>
+          </Text>
+        </Stack>
       </Stack>
-      <Stack as="footer" width="100%" align="center">
-        <Socials />
-        <Text>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <Link href="https://www.gatsbyjs.org">Gatsby</Link>
-        </Text>
-      </Stack>
-    </Stack>
-  )
-}
+    </Flex>
+  );
+};
 
-export default Layout
+export default Layout;
